1513cc62f0d379d99b51c44508604c16409bbbcb
yanghaiqiang <yanghaiqiang>
2018-06-14 17:06:51 +0800

    小程序测试包第一次提交

---
+++ pages/home/home.js
@@ -0,0 +1,28 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [     'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  jump:function(index){
+    console.log(index)
+  }
+})
