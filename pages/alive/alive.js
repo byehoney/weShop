5f2e0b9e404e4c055a1c72aa3d3c0e253c4d7034
yanghaiqiang <yanghaiqiang>
2018-06-19 18:36:54 +0800

    '增加直播'

--- pages/alive/alive.js
+++ pages/alive/alive.js
@@ -1,66 +1,84 @@
-// pages/alive/alive.js
 Page({
+    onReady(res) {
+        this.ctx = wx.createLivePlayerContext('player');
+        var socketOpen = false
+        var socketMsgQueue = []
+        wx.connectSocket({
+            url: 'test.php'
+        })
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+        wx.onSocketOpen(function (res) {
+            socketOpen = true
+            for (var i = 0; i < socketMsgQueue.length; i++) {
+                sendSocketMessage(socketMsgQueue[i])
+            }
+            socketMsgQueue = []
+        })
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+        function sendSocketMessage(msg) {
+            if (socketOpen) {
+                wx.sendSocketMessage({
+                    data: msg
+                })
+            } else {
+                socketMsgQueue.push(msg)
+            }
+        }
+    },
+    statechange(e) {
+        console.log('live-player code:', e.detail.code)
+    },
+    error(e) {
+        console.error('live-player error:', e.detail.errMsg)
+    },
+    bindPlay() {
+        this.ctx.play({
+            success: res => {
+                console.log('play success')
+            },
+            fail: res => {
+                console.log('play fail')
+            }
+        })
+    },
+    bindPause() {
+        this.ctx.pause({
+            success: res => {
+                console.log('pause success')
+            },
+            fail: res => {
+                console.log('pause fail')
+            }
+        })
+    },
+    bindStop() {
+        this.ctx.stop({
+            success: res => {
+                console.log('stop success')
+            },
+            fail: res => {
+                console.log('stop fail')
+            }
+        })
+    },
+    bindResume() {
+        this.ctx.resume({
+            success: res => {
+                console.log('resume success')
+            },
+            fail: res => {
+                console.log('resume fail')
+            }
+        })
+    },
+    bindMute() {
+        this.ctx.mute({
+            success: res => {
+                console.log('mute success')
+            },
+            fail: res => {
+                console.log('mute fail')
+            }
+        })
+    }
 })
